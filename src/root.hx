c6e250e935089929ef421ac6472c9d2496a86a9b
Nancy <nm389@nau.edu>
2015-01-28 13:13:36 -0700

    Ninja now reacts to being clicked on with animation
    he jumps and his face turns red. Also, a little bit of cleanup.

--- src/root.hx
+++ src/root.hx
@@ -9,10 +9,16 @@ import starling.text.TextField;
 import flash.system.System;
 import Math.random;
 import starling.utils.Color;
+import starling.textures.Texture;
+import starling.animation.Tween;
 
 class Root extends Sprite {
 	public static var assets:AssetManager;
-	public var ninja:Image;
+	
+	public var ninja:Sprite;
+	public var ninja_ow:Image;
+	public var ninja_norm:Image;
+	
 	public var lastTouch:Float=1.0;
 	public var score=0;
 	public var scoreTxt:TextField;
@@ -25,7 +31,7 @@ class Root extends Sprite {
 
 	public function start(startup:Startup){
 		assets=new AssetManager();
-		assets.enqueue("assets/Ninja.png");
+		assets.enqueue("assets/Ninja.png", "assets/Ninja_2.png");
 		assets.loadQueue(function onProgress(ratio:Float) {
 				// as assets get loaded, ratio gets updated. can be used for progress bar.
 				if (ratio == 1) {
@@ -33,21 +39,31 @@ class Root extends Sprite {
 					Starling.juggler.tween(startup.loadingBitmap, 2.0, {transition:Transitions.EASE_OUT, delay:0, alpha: 0, onComplete: function(){
 						// cleaning up the loadingScreen after it has already faded	
 						startup.removeChild(startup.loadingBitmap);
-							var quad = new Quad(20,20,0xababab);
-							quad.touchable = false;
-							quad.x = (flash.Lib.current.stage.stageWidth/2)-10;
-							quad.y = (flash.Lib.current.stage.stageHeight/2)-10;
-							quad.color = colors[Math.floor(random()*colors.length)];
-							addChild(quad);
+							
+						// background box
+						var quad = new Quad(20,20,0xababab);
+						quad.touchable = false;
+						quad.x = (flash.Lib.current.stage.stageWidth/2)-10;
+						quad.y = (flash.Lib.current.stage.stageHeight/2)-10;
+						quad.color = colors[Math.floor(random()*colors.length)];
+						addChild(quad);
 
-							ninja = new Image(Root.assets.getTexture("Ninja"));
-							ninja.touchable = true; // touchable must to true inorder for the Object to receive Touch Events
-							ninja.addEventListener("touch", onTouch); // Assigning the "touch" Event to onTouch
-							ninja.useHandCursor = true;
-							ninja.x=20;
-							ninja.y=20;
-							addChild(ninja);
-						}});
+						// the two ninja states
+						ninja_norm = new Image(Root.assets.getTexture("Ninja"));
+						ninja_ow = new Image(Root.assets.getTexture("Ninja_2"));
+							
+						ninja = new Sprite();
+						ninja.addChild(ninja_norm);
+						ninja.addChild(ninja_ow);
+						ninja_ow.visible = false;
+							
+						ninja.touchable = true; // touchable must to true inorder for the Object to receive Touch Events
+						ninja.addEventListener("touch", onTouch); // Assigning the "touch" Event to onTouch
+						ninja.useHandCursor = true;
+						ninja.x=20;
+						ninja.y=20;
+						addChild(ninja);
+					}});
 				}
 			});
 	}
@@ -68,9 +84,24 @@ class Root extends Sprite {
 				quad.y = (flash.Lib.current.stage.stageHeight/2)-(quad.height/2);
 				quad.color = colors[Math.floor(random()*colors.length)];
 
-				touch.target.x=random()*(flash.Lib.current.stage.stageWidth-touch.target.width);
-				touch.target.y=random()*(flash.Lib.current.stage.stageHeight-touch.target.height);
-				// ^ Jumps the "Ninja" to a Random Point on the Screen
+				// the ninja has been clicked, so his face turns red
+				ninja_norm.visible = false;
+				ninja_ow.visible = true;
+
+				// animation for jumping up
+				Starling.juggler.tween(ninja, .25, { 
+				y: ninja.y - 10});
+				
+				// animation for jumping down. On complete, move ninja to new location
+				Starling.juggler.tween(ninja, .2, {
+					y: ninja.y + 3, delay: .25, onComplete: function() {
+					ninja_norm.visible = true;
+					ninja_ow.visible = false;
+					ninja.x=random()*(flash.Lib.current.stage.stageWidth-ninja.width);
+					ninja.y=random()*(flash.Lib.current.stage.stageHeight-ninja.height);
+					}
+				});
+				
 
 				if(quad.width >= flash.Lib.current.stage.stageWidth){
 					/*var quad = new Quad(640, 640, 0x000000);
@@ -84,7 +115,7 @@ class Root extends Sprite {
 						addChild(endGame);
 					}});
 					Trying to get a white diamond behind the Game Over Text*/
-					endGame = new TextField(640,640, "Game Over!\n Your Time was " + (flash.Lib.getTimer()/1000) + " seconds!");
+					endGame = new TextField(640,640, "You Win!\n Your Time was " + (flash.Lib.getTimer()/1000) + " seconds!");
 					endGame.hAlign = "center";
 					endGame.vAlign = "center";
 					addChild(endGame);
