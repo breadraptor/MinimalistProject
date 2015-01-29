ad27cb11df20ee4d364a7c8fe3a961ac92eb7350
RobRas <bobbyras90@gmail.com>
2015-01-29 12:19:31 -0700

    Refactored the code
    Cleaned up our code by adding a Ninja and Background class. If this
    breaks any code you've been working on, feel free to revert to an older
    version. I also made the ninja start at a random location instead of a
    predefined one.

--- src/root.hx
+++ src/root.hx
@@ -17,17 +17,12 @@ import starling.animation.Tween;
 class Root extends Sprite {
 	public static var assets:AssetManager;
 	
-	public var clickSound:Sound;
-	
-	public var ninja:Sprite;
-	public var ninja_ow:Image;
-	public var ninja_norm:Image;
+	public var ninja:Ninja;
 	
 	public var lastTouch:Float=1.0;
 	public var score=0;
 	public var scoreTxt:TextField;
 	public var endGame:TextField;
-	var colors:Array<UInt> = [Color.RED, Color.MAROON, Color.YELLOW, Color.BLUE, Color.NAVY, Color.GREEN, Color.PURPLE, Color.AQUA, Color.FUCHSIA];
 
 	public function new() {
 		super();
@@ -43,93 +38,130 @@ class Root extends Sprite {
 					Starling.juggler.tween(startup.loadingBitmap, 2.0, {transition:Transitions.EASE_OUT, delay:0, alpha: 0, onComplete: function(){
 						// cleaning up the loadingScreen after it has already faded	
 						startup.removeChild(startup.loadingBitmap);
-							
-						// background box
-						var quad = new Quad(20,20,0xababab);
-						quad.touchable = false;
-						quad.x = (flash.Lib.current.stage.stageWidth/2)-10;
-						quad.y = (flash.Lib.current.stage.stageHeight/2)-10;
-						quad.color = colors[Math.floor(random()*colors.length)];
-						addChild(quad);
+						
+						addChild(Background.getFirstQuad());
 
-						// the two ninja states
-						ninja_norm = new Image(Root.assets.getTexture("Ninja"));
-						ninja_ow = new Image(Root.assets.getTexture("Ninja_2"));
-							
-						ninja = new Sprite();
-						ninja.addChild(ninja_norm);
-						ninja.addChild(ninja_ow);
-						ninja_ow.visible = false;
-							
-						ninja.touchable = true; // touchable must to true inorder for the Object to receive Touch Events
+						Ninja.init();
+						
+						ninja = new Ninja();
 						ninja.addEventListener("touch", onTouch); // Assigning the "touch" Event to onTouch
-						ninja.useHandCursor = true;
-						ninja.x=20;
-						ninja.y=20;
 						addChild(ninja);
 					}});
 				}
 			});
-		clickSound = new Sound();
-		clickSound.load(new URLRequest("assets/click.mp3"));
 	}
 
 	private function onTouch(e:TouchEvent){
 		var touch:Touch = e.getTouch(stage);
 		if(touch!=null){
 			if(touch.phase == TouchPhase.BEGAN) {
-				clickSound.play();
 				score+=Math.ceil((lastTouch/flash.Lib.getTimer())*4);
 				// ^ Supposed to reward more points for fast Clicks
 				lastTouch=flash.Lib.getTimer();
+				
+				ninja.clicked();
 
-				var quad = new Quad(20,20,0xffffff);
+				var quad = Background.getNextQuad(score);
 				addChildAt(quad,0);
-				quad.scaleX+=score;
-				quad.scaleY+=score;
-				quad.x = (flash.Lib.current.stage.stageWidth/2)-(quad.width/2);
-				quad.y = (flash.Lib.current.stage.stageHeight/2)-(quad.height/2);
-				quad.color = colors[Math.floor(random()*colors.length)];
 
-				// the ninja has been clicked, so his face turns red
-				ninja_norm.visible = false;
-				ninja_ow.visible = true;
-
-				// animation for jumping up
-				Starling.juggler.tween(ninja, .25, { 
-				y: ninja.y - 10});
-				
-				// animation for jumping down. On complete, move ninja to new location
-				Starling.juggler.tween(ninja, .2, {
-					y: ninja.y + 3, delay: .25, onComplete: function() {
-					ninja_norm.visible = true;
-					ninja_ow.visible = false;
-					ninja.x=random()*(flash.Lib.current.stage.stageWidth-ninja.width);
-					ninja.y=random()*(flash.Lib.current.stage.stageHeight-ninja.height);
-					}
-				});
-				
-
-				if(quad.width >= flash.Lib.current.stage.stageWidth){
-					/*var quad = new Quad(640, 640, 0x000000);
-					quad.rotation=(3.14/2);
-					quad.alpha=0;
-					addChild(quad);
-					Starling.juggler.tween(quad, 1.5, {transition:Transitions.EASE_OUT, delay:0, alpha: 1, onComplete: function(){
-						endGame = new TextField(640,640, "Game Over!\n Your Time was " + (flash.Lib.getTimer()/1000) + " seconds!");
-						endGame.hAlign = "center";
-						endGame.vAlign = "center";
-						addChild(endGame);
-					}});
-					Trying to get a white diamond behind the Game Over Text*/
-					endGame = new TextField(640,640, "You Win!\n Your Time was " + (flash.Lib.getTimer()/1000) + " seconds!");
-					endGame.hAlign = "center";
-					endGame.vAlign = "center";
-					addChild(endGame);
-				}
-
-
+				checkGameOver(quad);
 			}
 		}
+	}
+	private function checkGameOver(quad:Quad) {
+		if(quad.width >= flash.Lib.current.stage.stageWidth){
+			/*var quad = new Quad(640, 640, 0x000000);
+			quad.rotation=(3.14/2);
+			quad.alpha=0;
+			addChild(quad);
+			Starling.juggler.tween(quad, 1.5, {transition:Transitions.EASE_OUT, delay:0, alpha: 1, onComplete: function(){
+				endGame = new TextField(640,640, "Game Over!\n Your Time was " + (flash.Lib.getTimer()/1000) + " seconds!");
+				endGame.hAlign = "center";
+				endGame.vAlign = "center";
+				addChild(endGame);
+			}});
+			Trying to get a white diamond behind the Game Over Text*/
+			endGame = new TextField(640,640, "You Win!\n Your Time was " + (flash.Lib.getTimer()/1000) + " seconds!");
+			endGame.hAlign = "center";
+			endGame.vAlign = "center";
+			addChild(endGame);
+		}
+	}
+}
+
+class Ninja extends Sprite {
+	private static var clickSound:Sound;
+	
+	private var ow:Image;
+	private var norm:Image;
+	
+	public function new() {
+		super();
+	
+		// the two ninja states
+		norm = new Image(Root.assets.getTexture("Ninja"));
+		ow = new Image(Root.assets.getTexture("Ninja_2"));
+		
+		addChild(norm);
+		addChild(ow);
+		ow.visible = false;
+			
+		touchable = true; // touchable must to true inorder for the Object to receive Touch Events
+		useHandCursor = true;
+		x = random()*(flash.Lib.current.stage.stageWidth - width);
+		y = random()*(flash.Lib.current.stage.stageHeight - height);
+	}
+	
+	// Should only be called once
+	public static function init() {
+		clickSound = new Sound();
+		clickSound.load(new URLRequest("assets/click.mp3"));
+	}
+	
+	public function clicked() {
+		clickSound.play();
+	
+		// the ninja has been clicked, so his face turns red
+		norm.visible = false;
+		ow.visible = true;
+
+		// animation for jumping up
+		Starling.juggler.tween(this, .25, { 
+		y: this.y - 10});
+		
+		// animation for jumping down. On complete, move ninja to new location
+		Starling.juggler.tween(this, .2, {
+			y: this.y + 3, delay: .25, onComplete: function() {
+			norm.visible = true;
+			ow.visible = false;
+			x = random()*(flash.Lib.current.stage.stageWidth - width);
+			y = random()*(flash.Lib.current.stage.stageHeight - height);
+			}
+		});
+	}
+}
+
+// Static class to generate backgrounds
+class Background {
+	private static var colors:Array<UInt> = [Color.RED, Color.MAROON, Color.YELLOW, Color.BLUE, Color.NAVY, Color.GREEN, Color.PURPLE, Color.AQUA, Color.FUCHSIA];
+	
+	public static function getFirstQuad():Quad {
+		// background box
+		var quad = new Quad(20,20,0xababab);
+		quad.touchable = false;
+		quad.x = (flash.Lib.current.stage.stageWidth/2)-10;
+		quad.y = (flash.Lib.current.stage.stageHeight/2)-10;
+		quad.color = colors[Math.floor(random()*colors.length)];
+		return quad;
+	}
+	
+	public static function getNextQuad(score:Int):Quad {
+		var quad = new Quad(20,20,0xffffff);
+		quad.scaleX+=score;
+		quad.scaleY+=score;
+		quad.x = (flash.Lib.current.stage.stageWidth/2)-(quad.width/2);
+		quad.y = (flash.Lib.current.stage.stageHeight/2)-(quad.height/2);
+		quad.color = colors[Math.floor(random()*colors.length)];
+		return quad;
 	}
 }
